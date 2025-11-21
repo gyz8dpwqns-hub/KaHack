752fe48f32068262f253a423627617add96142e5
gyz8dpwqns-hub <gyz8dpwqns@privaterelay.appleid.com>
2025-11-21 14:02:35 -0800

    Update KaHack!.meta.js

--- KaHack!.meta.js
+++ KaHack!.meta.js
@@ -1,5 +1,5 @@
-// ==UserScript==
-// @name         KaHack!
+// ==UserScript== 5778292
+// @name         Damian A
 // @version      1.0.25
 // @namespace    https://github.com/jokeri2222
 // @description  A hack for kahoot.it!
